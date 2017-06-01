915800270c101e3f9146f0abfd72acd77be7541f
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-06-01 21:33:56 +0900

    hot fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
-  if Rails.env == 'production'
-    config.asset_host = 'http://52.197.6.123:3000'
-  end
+  # if Rails.env == 'production'
+  #   config.asset_host = 'http://52.197.6.123:3000'
+  # end
 end
 
