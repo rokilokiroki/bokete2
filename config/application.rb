6632243d2529f1e6731e387ed408f5b9091ab639
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-06-01 22:17:06 +0900

    hot fix

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Bokete2
   class Application < Rails::Application
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-    config.serve_static_assets = true
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
