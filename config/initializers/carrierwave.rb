8e8af91b45823d637ce376c0a4eaa91a99982955
やまもと <aiueo@aaaaa-no-MacBook-Air.local>
2017-06-09 16:14:09 +0900

    S3導入

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,27 @@
-CarrierWave.configure do |config|
-  # if Rails.env == 'production'
-  #   config.asset_host = 'http://52.197.6.123:3000'
-  # end
-end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'bokete2'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bokete2'
+
+    when 'test'
+        config.fog_directory  = 'bokete2'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bokete2'
+
+    when 'production'
+        config.fog_directory  = 'bokete2'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bokete2'
+    end
+end
